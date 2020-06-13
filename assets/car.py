9c8bdccf2da6113d2b8520645cc06a14a1d709ee
Dodilei <mauricioandrade2002@gmail.com>
2020-06-13 19:33:13 -0400

    Update

--- assets/car.py
+++ assets/car.py
@@ -7,19 +7,6 @@ class Car(SVGMobject):
         "stroke_color": WHITE
         }
 
-    index_names = {
-        "rear": (0,),
-        "front": (1,),
-        "bump": (2,),
-        "tire_light": (3, 7),
-        "tire_shadow": (4, 8),
-        "wheel_light": (5, 9),
-        "wheel_shadow": (6, 10),
-        "windows": (11, 12),
-        "light1": (13,),
-        "light2": (14,)
-        
-    }
 
     def __init__(self, **kwargs):
         svg_file = os.path.join(os.getcwd(), "assets", "car.svg")
@@ -39,35 +26,8 @@ class Car(SVGMobject):
             for k in keys:
                 indexes += [*Car.index_names[k]]
             return indexes
-
-        for i in flatten_index("rear", "bump"):
-            self.submobjects[i].set_fill("#78b9eb", opacity = opacity)
-
-        for i in flatten_index("front"):
-            self.submobjects[i].set_fill("#5a8bb0", opacity = opacity)
-
-        for i in flatten_index("tire_light"):
-            self.submobjects[i].set_fill("#616064", opacity = opacity)
-
-        for i in flatten_index("tire_shadow"):
-            self.submobjects[i].set_fill("#565659", opacity = opacity)
-
-        for i in flatten_index("wheel_light"):
-            self.submobjects[i].set_fill("#cdcdd0", opacity = opacity)
-
-        for i in flatten_index("wheel_shadow"):
-            self.submobjects[i].set_fill("#acabb1", opacity = opacity)
-
-        for i in flatten_index("windows"):
-            self.submobjects[i].set_fill("#ffffff", opacity = opacity)
-
-        for i in flatten_index("light1"):
-            self.submobjects[i].set_fill("#ff5023", opacity = opacity)
-
-        for i in flatten_index("light2"):
-            self.submobjects[i].set_fill("#ff9811", opacity = opacity)
-
-
+        
+        self.set_color(WHITE)
 
         self.set_stroke(color = stroke_color, opacity = stroke_opacity)
 
